ce220bd935991aba2511f6712e9a52d1bd72c5ae
Nataly Goncalves <nataly.bio@gmail.com>
2021-05-05 16:02:04 +0100

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">Developed by <a href="https://github.com/NatalyPG" target="_blank" rel="noreferrer">Nataly Goncalves</a></p>
+                <p className="text-light"><small><strong>Spring React Weak</strong><br />
+            Event promoted by DevSuperior school <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
